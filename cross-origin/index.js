f94aa031a5e792b9a1091484af7cd93df7043fd2
xirc <xirckey@gmail.com>
2020-09-28 21:02:48 +0900

    Attempt to request to different origins.

---
+++ cross-origin/index.js
@@ -0,0 +1,21 @@
+const button = document.getElementsByTagName("button")[0];
+
+button.addEventListener("click", function() {
+  getACookie().then(() => getData());
+});
+
+function getACookie() {
+  return fetch("http://localhost:5000/get-cookie/").then(response => {
+    // make sure to check response.ok in the real world!
+    return Promise.resolve("All good, fetch the data");
+  });
+}
+
+function getData() {
+  fetch("http://localhost:5000/api/cities/")
+    .then(response => {
+      // make sure to check response.ok in the real world!
+      return response.json();
+    })
+    .then(json => console.log(json));
+}
