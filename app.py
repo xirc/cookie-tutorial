d5ccfc9f3f75853dc1bbca278ffb4de4060a2d6d
xirc <xirckey@gmail.com>
2020-09-28 20:30:13 +0900

    Use the Cookie with Path attribute.

--- app.py
+++ app.py
@@ -1,13 +1,25 @@
-from flask import Flask, make_response
+from flask import Flask, make_response, request
 
 app = Flask(__name__)
 
 
-@app.route('/index/', methods=["GET"])
+@app.route('/', methods=["GET"])
 def index():
     response = make_response("Here, take some cookie!")
-    response.headers["Set-Cookie"] = "my-first-cookie=some-cookie-value; HttpOnly"
+    response.set_cookie(key="id", value="3db4adj3d", path="/about/")
     return response
+
+
+@app.route("/about/", methods=["GET"])
+def about():
+    print(request.cookies)
+    return "Hello world!"
+
+
+@app.route("/contact/", methods=["GET"])
+def contact():
+    print(request.cookies)
+    return "Hello world!"
 
 
 if __name__ == '__main__':
