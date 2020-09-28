6123d57026733035fd3d4e50315598616fe98eca
xirc <xirckey@gmail.com>
2020-09-28 19:58:41 +0900

    Use HttpOnly attribute for prohibiting access from JavaScript.

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 @app.route('/index/', methods=["GET"])
 def index():
     response = make_response("Here, take some cookie!")
-    response.headers["Set-Cookie"] = "my-first-cookie=some-cookie-value"
+    response.headers["Set-Cookie"] = "my-first-cookie=some-cookie-value; HttpOnly"
     return response
 
 
