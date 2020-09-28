2fe35703f462c9ee98a657521fcfa2e79e785464
xirc <xirckey@gmail.com>
2020-09-28 19:56:54 +0900

    Add the simple cookie example.

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask, make_response
+
+app = Flask(__name__)
+
+
+@app.route('/index/', methods=["GET"])
+def index():
+    response = make_response("Here, take some cookie!")
+    response.headers["Set-Cookie"] = "my-first-cookie=some-cookie-value"
+    return response
+
+
+if __name__ == '__main__':
+    app.run()
